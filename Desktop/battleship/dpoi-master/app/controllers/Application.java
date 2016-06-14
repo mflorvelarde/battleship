a33427a8118c2cc6bfd73cf12481d35d251183ac
mflorvelarde <maria.velarde@ing.austral.edu.ar>
2016-06-14 12:08:57 -0300

    drag&drop y rotacion de los barcos y tablero para hacer dispartos

--- Desktop/battleship/dpoi-master/app/controllers/Application.java
+++ Desktop/battleship/dpoi-master/app/controllers/Application.java
@@ -3,8 +3,10 @@ package controllers;
 import play.db.ebean.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.game;
 import views.html.home;
 import views.html.index;
+import views.html.login;
 
 public class Application extends Controller {
 
@@ -16,5 +18,14 @@ public class Application extends Controller {
     @Transactional
     public static Result home() {
         return ok(home.render());
+    }
+
+    @Transactional
+    public static Result play() {
+        return ok(game.render());
+    }
+
+    public static Result login() {
+        return ok(login.render());
     }
 }
